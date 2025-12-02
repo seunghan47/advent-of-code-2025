b0d31e0c49785ceff6c6b22c11cb43efc82a306b
Paul Lim <paulslim47@gmail.com>
2025-12-01 19:42:04 -0500

    final solution

--- day1/src/solution.java
+++ day1/src/solution.java
@@ -3,23 +3,32 @@ import java.io.FileReader;
 
 public class solution {
     public static void main(String[] args) {
-        String fp = "src/input2.txt";
+        String fp = "src/input2";
         int start = 50;
         int ans = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(fp))){
             String line;
             while ((line = br.readLine()) != null) {
                 char direction = line.charAt(0);
-                int amount = Integer.parseInt(line.substring(1)) % 100;
+                int amount = Integer.parseInt(line.substring(1));
                 if (direction == 'R') {
-                    start += amount;
-                    start = start % 100;
+                    ans += (start + amount) / 100;
+                    start = (start + amount) % 100;
                 } else {
-                    start -= amount;
-                    start = start % 100;
-                    if (start < 0) start += 100;
+                    if (amount > start) {
+                        if (start == 0) ans --;
+                        start -= amount;
+                        ans += (int) Math.ceil((double) start / -100);
+                        start = start % 100;
+                        if (start < 0) start += 100;
+                        if (start == 0) ans++;
+                    } else if (amount == start) {
+                        ans++;
+                        start = 0;
+                    } else {
+                        start -= amount;
+                    }
                 }
-                if (start == 0) ans++;
             }
         } catch (Exception e) {
             System.out.println("hello + "  + e.getMessage());
